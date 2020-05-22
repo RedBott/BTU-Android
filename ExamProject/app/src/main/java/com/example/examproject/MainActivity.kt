e703ceb3fbf1a9fdf2a9270b0e34732b00d71043
RedBott <andrei.tsitsvidze.1@btu.edu.ge>
2020-05-22 22:17:44 +0400

    Final final commit

--- ExamProject/app/src/main/java/com/example/examproject/MainActivity.kt
+++ ExamProject/app/src/main/java/com/example/examproject/MainActivity.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
 import android.widget.Toast
+import androidx.annotation.NonNull
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
@@ -87,11 +93,15 @@ class MainActivity : AppCompatActivity() {
         DataLoader.getRequest(jokeTheme,object :CustomCallback{
             override fun onSuccess(result: String) {
                 val model :JokesModel = Gson().fromJson(result,JokesModel::class.java)
-                var db: FirebaseFirestore? = FirebaseFirestore.getInstance()
+                val db: FirebaseFirestore = FirebaseFirestore.getInstance()
                 val joke = hashMapOf<String,Any>()
                 joke["Question"] = model.setup
                 joke["Answer"] = model.delivery
                 joke["Id"] = model.id
+                db.collection("Jokes")
+                    .add(joke)
+                    .addOnSuccessListener { Log.d("TAG", "DocumentSnapshot successfully written!") }
+                    .addOnFailureListener { e -> Log.w("TAG", "Error writing document", e) }
 
             }
         })
